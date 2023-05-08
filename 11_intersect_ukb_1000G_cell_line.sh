de6a42f9d1eca6e87517b6534d72521460414461
astheeggeggs <duncan.stuart.palmer@gmail.com>
2023-05-08 13:44:25 +0100

    commented to move files

--- 11_intersect_ukb_1000G_cell_line.sh
+++ 11_intersect_ukb_1000G_cell_line.sh
@@ -17,9 +17,9 @@ ukb_imputed_subset_dir="/well/lindgren/UKBIOBANK/dpalmer/PRS_cell_data/data/UKB_
 
 chr=${SLURM_ARRAY_TASK_ID}
 
-# if [ $chr -eq 23 ]; then
-# 	chr="X"
-# fi
+if [ $chr -eq 23 ]; then
+	chr="X"
+fi
 
 # bcftools isec -c none -n=3 \
 # -p ${ukb_imputed_subset_dir}/UKB_1000G_cell_line_intersection_chr${chr} -O z \
