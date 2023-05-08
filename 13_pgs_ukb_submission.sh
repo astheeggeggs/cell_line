7555b7357642382ffd2c5830a72e25889779196b
astheeggeggs <duncan.stuart.palmer@gmail.com>
2023-05-08 14:59:49 +0100

    typo

--- 13_pgs_ukb_submission.sh
+++ 13_pgs_ukb_submission.sh
@@ -16,7 +16,7 @@ mkdir -p ${pgs_results_dir}/ukb_job_${SLURM_ARRAY_TASK_ID}
 
 pgs-calc apply \
 	--ref ${pgs_score_dir}/pgs-catalog-20221123-hg19/scores/job_${SLURM_ARRAY_TASK_ID}.txt \
-	${ukb_imputed_intersected_1000G_subset_dir}/UKB_imputed_subset_chr$*_thresholded_recode_merge_typed_final_UKB_cell_line_1000G_intersected.vcf.gz \
+	${ukb_imputed_intersected_1000G_subset_dir}/UKB_imputed_subset_chr*_thresholded_recode_merge_typed_final_UKB_cell_line_1000G_intersected.vcf.gz \
 	--out ${pgs_results_dir}/ukb_job_${SLURM_ARRAY_TASK_ID}/ukb_intersect_1000G_cell_line_job_${SLURM_ARRAY_TASK_ID}_scores.txt \
 	--dbsnp /well/lindgren/UKBIOBANK/dpalmer/PRS_cell_data/data/dbsnp154_hg19.txt.gz \
 	--report-html ${pgs_results_dir}/ukb_job_${SLURM_ARRAY_TASK_ID}/ukb_intersect_1000G_cell_line_job_${SLURM_ARRAY_TASK_ID}_scores_report.html \
