92184b0dd216f54294985c8c4f786680462334cf
astheeggeggs <duncan.stuart.palmer@gmail.com>
2023-05-30 19:08:40 +0100

    updated cell line filtering to allow haploid X samples to be scored

--- 03_filter_cell_lines.sh
+++ 03_filter_cell_lines.sh
@@ -28,7 +28,15 @@ if [ $chr -eq 23 ]; then
 	bcftools index -f ${imputed_path}/chr${chr}.dose.vcf.gz
 	bcftools isec -c none -n=2 -w1 \
 	-O z -o ${imputed_path}/chr${chr}.dose.subset.vcf.gz \
-	${imputed_path}/chr${chr}.dose.vcf.gz ${input}_chr${chr}.vcf.gz 
+	${imputed_path}/chr${chr}.dose.vcf.gz ${input}_chr${chr}.vcf.gz
+	bcftools convert -h ${input}_test --haploid2diploid ${input}_chr${chr}.vcf.gz
+	mkdir ${phase3_1kg_path}/original_X
+	cp ${input}_chr${chr}.vcf.gz ${phase3_1kg_path}/original_X/all_phase3_r2_0.3_chr${chr}_hap_and_dip.vcf.gz
+	cp ${input}_chr${chr}.vcf.gz.csi ${phase3_1kg_path}/original_X/all_phase3_r2_0.3_chr${chr}_hap_and_dip.vcf.gz.csi
+	bcftools convert -H ${input}_test -o test.vcf.gz -O z
+	bcftools annotate --set-id +'%CHROM:%POS:%REF:%FIRST_ALT' test.vcf.gz -o ${input}_chr${chr}.vcf.gz -O z
+	bcftools index -f ${input}_chr${chr}.vcf.gz
+	bcftools index -f ${imputed_path}/chr${chr}.dose.subset.vcf.gz
 else
 	# bgzip -f ${input}_chr${chr}.vcf
 	bcftools index -f ${input}_chr${chr}.vcf.gz
@@ -36,4 +44,5 @@ else
 	bcftools isec -c none -n=2 -w1 \
 	-O z -o ${imputed_path}/chr${chr}.dose.subset.vcf.gz \
 	${imputed_path}/chr${chr}.dose.vcf.gz ${input}_chr${chr}.vcf.gz
+	bcftools index -f ${imputed_path}/chr${chr}.dose.subset.vcf.gz
 fi
