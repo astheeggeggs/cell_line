0ab1bfd56fc96cf1b0a18f6892408dcdee5f8f9d
astheeggeggs <duncan.stuart.palmer@gmail.com>
2023-05-31 13:12:55 +0100

    updated output dir of pgs to avoid overwriting

--- 12_pgs_1000G_submission.sh
+++ 12_pgs_1000G_submission.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
 #SBATCH -J 1000G-prs
 #SBATCH -A lindgren.prj 
-#SBATCH -o output.out 
-#SBATCH -e error.err 
+#SBATCH -o 1000G.full.int.prs.output.%A_%a.out
+#SBATCH -e 1000G.full.int.prs.error.%A_%a.err 
 #SBATCH -c 4 
 #SBATCH -p short
 #SBATCH --array 1-33:1 
@@ -12,7 +12,7 @@ export PATH="/well/lindgren/dpalmer/:$PATH"
 pgs_results_dir="/well/lindgren/UKBIOBANK/dpalmer/PRS_cell_data/data/PGS_score_results"
 pgs_score_dir="/well/lindgren/UKBIOBANK/dpalmer/PRS_cell_data/data/PGS_score_files"
 phase3_1kg_path="/well/lindgren/UKBIOBANK/dpalmer/PRS_cell_data/data/1000G/B37"
-mkdir ${pgs_results_dir}/job_${SLURM_ARRAY_TASK_ID}
+mkdir -p ${pgs_results_dir}/job_${SLURM_ARRAY_TASK_ID}
 
 pgs-calc apply \
 	--ref ${pgs_score_dir}/pgs-catalog-20221123-hg19/scores/job_${SLURM_ARRAY_TASK_ID}.txt \
